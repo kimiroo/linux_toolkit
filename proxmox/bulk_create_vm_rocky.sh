4477d6bbff9ed1d513fac6bc6b2a4a449a1a9232
kimiroo <43537287+kimiroo@users.noreply.github.com>
2026-05-20 21:31:42 +0900

    Redue VM count Increase RAM Change qcow2 to raw

--- proxmox/bulk_create_vm_rocky.sh
+++ proxmox/bulk_create_vm_rocky.sh
@@ -8,19 +8,21 @@ CORES=1
 RAM=4096
 GATEWAY="10.5.0.254"
 
-declare -a RAM_SIZES=(2048 4096 4096) #master, worker, worker or docker
+declare -a CORES_LIST=(1 2) # master, worker
+declare -a RAM_LIST=(4096 8192) # master, worker
 
-for i in 1 2 3
+for i in 1 2
 do
     VM_ID="${NODE_ID_PREFIX}0${i}"
     VM_NAME=$(printf "rocky-node%02d-vm%02d" "$NODE_ID_PREFIX" "$i")
     VM_IP="10.5.47.${NODE_ID_PREFIX}${i}/16"
-    RAM=${RAM_SIZES[$((i-1))]}
+    CORES=${CORES_LIST[$((i-1))]}
+    RAM=${RAM_LIST[$((i-1))]}
 
     echo "Creating VM $VM_NAME (ID: $VM_ID, RAM: ${RAM}MB)..."
 
     echo "Cloning template..."
-    qm clone $TEMPLATE_ID $VM_ID --full --storage $STORAGE --name $VM_NAME --format qcow2
+    qm clone $TEMPLATE_ID $VM_ID --full --storage $STORAGE --name $VM_NAME --format raw
 
     echo "Configuring VM..."
     qm set $VM_ID --vcpus $CPUS --cores $CORES
@@ -28,14 +30,5 @@ do
     qm set $VM_ID --ipconfig0 ip=$VM_IP,gw=$GATEWAY
     qm set $VM_ID --onboot 1
 done
-
-#for i in 1 2 3
-#do
-#    VM_ID="${NODE_ID_PREFIX}0${i}"
-#    VM_NAME=$(printf "rocky-node%02d-vm%02d" "$NODE_ID_PREFIX" "$i")
-#
-#    echo "Starting VM $VM_NAME (ID: $VM_ID)..."
-#    qm start $VM_ID
-#done
 
 echo "Done."
--- proxmox/bulk_rename_vm_rocky.sh
+++ proxmox/bulk_rename_vm_rocky.sh
@@ -5,20 +5,17 @@ read -p "Node ID: " NODE_ID_PREFIX
 if [ "$NODE_ID_PREFIX" == "1" ]; then
     qm set 101 --name rocky-master-01 --tags "k3s-master,node-01"
     qm set 102 --name rocky-worker-01 --tags "k3s-worker,node-01"
-    qm set 103 --name rocky-worker-02 --tags "k3s-worker,node-01"
 
 elif [ "$NODE_ID_PREFIX" == "2" ]; then
     qm set 201 --name rocky-master-02 --tags "k3s-master,node-02"
-    qm set 202 --name rocky-worker-03 --tags "k3s-worker,node-02"
-    qm set 203 --name rocky-worker-04 --tags "k3s-worker,node-02"
+    qm set 202 --name rocky-worker-02 --tags "k3s-worker,node-02"
 
 elif [ "$NODE_ID_PREFIX" == "3" ]; then
     qm set 301 --name rocky-master-03 --tags "k3s-master,node-03"
-    qm set 302 --name rocky-worker-05 --tags "k3s-worker,node-03"
-    qm set 303 --name rocky-docker-01 --tags "docker,node-03" --cores 2 # Special config for Docker VM
+    qm set 302 --name rocky-worker-03 --tags "k3s-worker,node-03"
 fi
 
-for vm_idx in 1 2 3
+for vm_idx in 1 2
 do
     VM_ID=$(printf "${NODE_ID_PREFIX}0${vm_idx}")
     echo "Starting VM $VM_ID..."
