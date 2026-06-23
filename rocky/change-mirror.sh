8fe1bbf40e419fe9de907ba6e63637f93fce158b
Yongjun Kim <43537287+kimiroo@users.noreply.github.com>
2026-06-23 18:15:45 +0900

    Add rocky linux change-mirror.sh

---
+++ rocky/change-mirror.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+METHOD="mirrorlist"
+BASEURL="https://mirror.navercorp.com/rocky"
+
+# Parse options using getopts
+# e:p: means option -e and -p require an argument
+while getopts "" opt; do
+    case " ${opt} " in
+        m) METHOD="$OPTARG" ;;
+        b) BASEURL="$OPTARG" ;;
+        *) echo "Invalid option"; exit 1 ;;
+    esac
+done
+
+for file in /etc/yum.repos.d/*.repo; do
+
+    # Process the file here
+    echo "Processing $file"
+
+    if []; then
+        sed -i.bak 's|mirrorlist|#mirrorlist|g' $file
+        sed -i 's|#baseurl|baseurl|g' $file
+        sed -i 's|http://dl.rockylinux.org/$contentdir|'"${BASEURL}"'|g' $file
+
+    elif []; then
+        sed -i.bak 's|^#mirrorlist|mirrorlist|g' $file
+        sed -i 's|^baseurl|#baseurl|g' $file
+        sed -i 's|^mirrorlist.*|&\&country=KR|g' $file
+    fi
+
+done
